6ad49be16c775e91341f94ad2400c8dff8ff9e1d
Takeshi Sugiyama <kawacub@gmail.com>
2020-06-06 09:23:00 +0900

    change Lower numpad to normal

--- keyboards/planck/keymaps/myplanck/keymap.c
+++ keyboards/planck/keymaps/myplanck/keymap.c
@@ -87,7 +87,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |Shift |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   Up |'/SFT |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |ZH/Ctl|Adjust| Alt  | GUI  |英数/L|    Space    |かな/R|  /    | Left | Dow  |Right |
+ * |ZH/Ctl|Adjust| Alt  | GUI  |英数/L |   Space     |かな/R|  /   | Left | Dow  |Right |
  * `-----------------------------------------------------------------------------------'
  */
 [_JIS] = LAYOUT_planck_grid(
@@ -107,13 +107,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Lower
  * ,-----------------------------------------------------------------------------------.
- * |Esc/FN1|   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
+ * |Esc/f1|   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      | Vol- | Vol+ | Mute |   .  |   -  |   4  |   5  |   6  |   *  |      |
+ * |Tab/f2|  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   4  |   5  |   6  |   /  |   -  |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |      | Prev | Next | Play |   ,  |   +  |   1  |   2  |   3  |   /  |   =  |
+ * |Shift |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |   1  |   2  |   3  |   *  |   +  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |   0  |      |      |      |      |
+ * |ZH/Ctl|Adjust| Alt  | GUI  |英数/L |    Space    |かな/R|   0  |   .  |   =   |Enter|
  * `-----------------------------------------------------------------------------------'
  */
 /*
@@ -126,8 +126,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 */
 [_LOWER] = LAYOUT_planck_grid(
   FN1_ESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-  FN2_TAB, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_P4,   KC_P5,   KC_P6,   KC_PSLS, KC_MINS,
-  KC_LSFT, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_P1,   KC_P2,   KC_P3,   KC_PAST, KC_PPLS,
+  FN2_TAB, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_4,    KC_5,    KC_6,   KC_PSLS, KC_MINS,
+  KC_LSFT, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_1,    KC_2,    KC_3,   KC_PAST, KC_PPLS,
   CTL_ZH,  ADJUST,  KC_LALT, KC_LGUI, M_EMHL,  KC_SPC,  KC_SPC,  M_KHRU,  KC_P0,   KC_PDOT, KC_PEQL, KC_PENT
 ),
 
